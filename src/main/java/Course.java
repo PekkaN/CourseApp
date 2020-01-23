cf67562e544a4710dd56f6e87b05f667e621d91d
Robin Blomberg <robin.blomberg@gmail.com>
2020-01-23 11:16:36 +0200

    On branch Robin  Changes to be committed: 	modified:   pom.xml 	new file:   src/main/java/Course.java

---
+++ src/main/java/Course.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Course {
+    private int courseCode;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String location;
+    private String materialType;
+    private String description;
+
+}
