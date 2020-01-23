625d2b5e614c9d3a1d9d41d38c1cd01d045334cf
Robin Blomberg <robin.blomberg@gmail.com>
2020-01-23 11:36:05 +0200

    Luokka Course konstruktorilla

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.List;
 
 public class Course {
     private int courseCode;
@@ -8,5 +9,22 @@ public class Course {
     private String location;
     private String materialType;
     private String description;
+    private String mathCode;
+    private String mainCategory;
+    private List<String> subCategories;
+
+    public Course(int courseCode, String name, LocalDate startDate, LocalDate endDate, String location, String materialType, String description, String mathCode, String mainCategory, List<String> subCategories){
+        this.courseCode = courseCode;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.materialType = materialType;
+        this.description = description;
+        this.mathCode = mathCode;
+        this.mainCategory = mainCategory;
+        this.subCategories = subCategories;
+    }
+
 
 }
